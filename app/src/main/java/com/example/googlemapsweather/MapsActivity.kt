52e133a5899b51d5c267303f474a0e22232dce0e
Goncharov01 <mr.sacha.goncharov.01@mail.ru>
2021-02-02 10:50:14 +0300

    location search

--- app/src/main/java/com/example/googlemapsweather/MapsActivity.kt
+++ app/src/main/java/com/example/googlemapsweather/MapsActivity.kt
@@ -1,10 +1,22 @@
 package com.example.googlemapsweather
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationServices.FusedLocationApi
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -13,10 +25,12 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.*
 
-
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var googleApiClient: GoogleApiClient
+    private lateinit var locationRequest: LocationRequest
+    private val REQUEST_LOCATION_PERMISSION = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +43,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
-//        !!
         mMap = googleMap!!
 
         val grodno = LatLng(53.6803665, 23.8290348)
@@ -37,12 +50,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.moveCamera(CameraUpdateFactory.newLatLng(grodno))
         mMap.setMinZoomPreference(12.0F)
         mMap.setMaxZoomPreference(20.0F)
+
+        mMap.setOnMyLocationButtonClickListener(object : GoogleMap.OnMyLocationButtonClickListener {
+
+            override fun onMyLocationButtonClick(): Boolean {
+
+                weather.isEnabled = true
+
+                return false
+            }
+        })
+
+        enableMyLocation()
+
     }
 
     fun searchLocation(view: View) {
-        println("qwwwwww" + editText.text)
 
         if (!editText.text.toString().equals("") || editText.text.toString() != null) {
+
+            weather.isEnabled = true
 
             var geocoder: Geocoder = Geocoder(this)
 
@@ -59,6 +86,45 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             mMap.setMaxZoomPreference(20.0F)
 
         }
+
+    }
+
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+
+    private fun enableMyLocation() {
+
+        if (isPermissionGranted()) {
+            mMap.isMyLocationEnabled = true
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            }
+        }
+    }
+
+    fun goIntent(view: View) {
+
+        val intent = Intent(this, WeatherActivity::class.java)
+        startActivity(intent)
 
     }
 
